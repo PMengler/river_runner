f3a199dbe6757c4c0a18c2fce3e1a9bf877e3f90
Savannah Vel <savannah.vel@vivint.com>
2022-12-05 22:19:42 -0800

    merged in FE branch

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,6 +3,59 @@ var fetchButton = document.getElementById('fetch-button');
 var locations = [];
 //getApi function is called when the fetchButton is clicked
 
+var geocoder;
+var map;
+var infowindow;
+
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var loca = new google.maps.LatLng(41.7475, -74.0872);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    // mapTypeId: google.maps.MapTypeId.ROADMAP,
+    center: loca,
+    zoom: 8
+  });
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+//   var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'mouseover', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
+function codeAddress() {
+  var address = document.getElementById('address').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == 'OK') {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+
+
 function riverRunner() {
   // Insert the API url to get a list of your repos
   var requestUrl = 'https://waterwatch.usgs.gov/webservices/realtime?region=or&format=json';
@@ -66,3 +119,4 @@ function riverRunner() {
   };
 
 fetchButton.addEventListener('click', riverRunner);
+google.maps.event.addDomListener(window, 'load', initialize);
